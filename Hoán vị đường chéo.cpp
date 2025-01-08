4eb59e35843be7bb38da68f745514dc70d5c7ded
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-01-08 18:21:17 +0700

    Create Hoán vị đường chéo.cpp

---
+++ Hoán vị đường chéo.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+#include <numeric>
+#include <climits>
+#include <iomanip>
+using namespace std;
+const int mod = 1e9 + 7;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin >> n;
+	vector<vector<int>> a(n, vector<int>(n));
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> a[i][j];
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		int temp = a[i][i];
+		a[i][i] = a[i][n - i - 1];
+		a[i][n - i - 1] = temp;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
