24ecf1f05ffdad5568edc3406e2ac0104d432a61
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-01-09 18:59:00 +0700

    Create Sắp xếp hàng của ma trận.cpp

---
+++ Sắp xếp hàng của ma trận.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+#include <numeric>
+#include <climits>
+#include <iomanip>
+#include <string>
+using namespace std;
+const int mod = 1e9 + 7;
+int dx[8] = { -1,-1,-1,0,0,1,1,1 };
+int dy[8] = { -1,0,1,-1,1,-1,0,1 };
+void sapxep(vector<int> &a, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		int min = i;
+		for (int j = i + 1; j < n; j++)
+		{
+			if (a[min] > a[j])
+			{
+				min = j;
+			}
+		}
+		swap(a[min], a[i]);
+	}
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin >> n;
+	vector<vector<int>> a(n, vector<int>(n));
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> a[i][j];
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		sapxep(a[i], n);
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
