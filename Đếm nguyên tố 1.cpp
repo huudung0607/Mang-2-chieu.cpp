6d4744807ebd59cdfa07801763c91e7b5f4b77f5
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-01-09 18:32:22 +0700

    Create Đếm nguyên tố 1.cpp

---
+++ Đếm nguyên tố 1.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+#include <numeric>
+#include <climits>
+#include <iomanip>
+#include <string>
+using namespace std;
+const int mod = 1e9 + 7;
+int dx[8] = { -1,-1,-1,0,0,1,1,1 };
+int dy[8] = { -1,0,1,-1,1,-1,0,1 };
+int a[505][505];
+bool prime(int n)
+{
+	if (n < 2) return false;
+	for (int i = 2; i <= sqrt(n); i++)
+	{
+		if (n % i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> a[i][j];
+		}
+	}
+	int dem = 0;
+	if (n % 2 == 0)
+	{
+		int dem = 0;
+		for (int i = 0; i < n; i++)
+		{
+			if (prime(a[i][i]))
+			{
+				dem++;
+			}
+			if (prime(a[i][n - i - 1]))
+			{
+				dem++;
+			}
+		}
+		cout << dem;
+	}
+	else
+	{
+		int num = a[n / 2][n / 2];
+		if (prime(num))
+		{
+			dem++;
+			a[n/2][n/2] = 0;
+		}
+		for (int i = 0; i < n; i++)
+		{
+			if (prime(a[i][i]))
+			{
+				dem++;
+			}
+			if (prime(a[i][n - i - 1]))
+			{
+				dem++;
+			}
+		}
+		cout << dem;
+	}
+	
+}
