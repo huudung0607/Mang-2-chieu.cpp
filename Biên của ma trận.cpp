0921d4c3f473963d553d0e7eb48d80110f50f126
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-01-10 09:53:59 +0700

    Create Biên của ma trận.cpp

---
+++ Biên của ma trận.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+#include <numeric>
+#include <climits>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int dx[8] = { -1,-1,-1,0,0,1,1,1 };
+int dy[8] = { -1,0,1,-1,1,-1,0,1 };
+int a[505][505];
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> a[i][j];
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if ( i == 0 || j == 0 || i == n - 1 || j == n - 1)
+			{
+				cout << a[i][j] << " ";
+			}
+		}
+	}
+}
