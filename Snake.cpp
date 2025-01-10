dd276633f6b06e201c7c82063792aea746d07d91
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-01-10 09:52:44 +0700

    Create Snake.cpp

---
+++ Snake.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+#include <numeric>
+#include <climits>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int dx[8] = { -1,-1,-1,0,0,1,1,1 };
+int dy[8] = { -1,0,1,-1,1,-1,0,1 };
+int a[505][505];
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> a[i][j];
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		if (i % 2 == 0)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				cout << a[i][j] << " ";
+			}
+			cout << endl;
+		}
+		else
+		{
+			for (int j = n - 1; j >= 0; j--)
+			{
+				cout << a[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
+}
